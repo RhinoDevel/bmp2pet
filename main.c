aaa2b1626a48d64f7d8fe65a00621e588a47d8d6
Marc <RhinoDevel@users.noreply.github.com>
2020-09-21 11:50:05 +0200

    Printing bmp. monochromatically.

--- main.c
+++ main.c
@@ -10,6 +10,30 @@
 #include "FileSys.h"
 #include "Bmp.h"
 
+static void print_bmp(struct Bmp const * const b)
+{
+	static char const fg = 'O';
+	static char const bg = '.';
+	
+	for(int row = 0;row < b->d.h;++row)
+    {
+        unsigned char * const row_ptr = b->p + 3 * row * b->d.w;
+
+        for(int col = 0;col < b->d.w;++col)
+        {
+            unsigned char * const col_ptr = row_ptr + 3 * col;
+			char c = fg;
+			
+			if(col_ptr[0] == 0 && col_ptr[1] == 0 && col_ptr[2] == 0)
+			{
+				c = bg;
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+	}
+}
+
 int main(int argc, char* argv[])
 {
     if(argc != 3)
@@ -31,6 +55,8 @@ int main(int argc, char* argv[])
     Sys_log_line(
         true, true, "Bitmap resolution = %d x %d pixels.", bmp->d.w, bmp->d.h);
         
+	print_bmp(bmp);
+		
 	// TODO: Implement!
 		
     Bmp_delete(bmp);
